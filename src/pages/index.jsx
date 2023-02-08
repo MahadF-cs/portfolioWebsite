e485262ba9beb64c4f7881c02f92cabcaf1c6145
Mahad Farah <mahadadanfarah@gmail.com>
2023-02-07 20:09:41 -0500

    Changed Favicon and add tracking

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,8 +10,13 @@ import {
 import { NextSeo } from "next-seo";
 import NextImage from "next/image";
 import Typewriter from "typewriter-effect";
+import ReactGA from "react-ga";
 
 import { seo, data } from "config";
+
+// Google Analytics Tracking ID
+const TRACKING_ID = "352903425"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 const Home = () => {
   const color = useColorModeValue("telegram.500", "telegram.400");
